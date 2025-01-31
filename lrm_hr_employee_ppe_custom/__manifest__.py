d5db8ac4950783e64084c1cd57636b1642fbd21e
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2025-01-31 18:50:48 -0300

    [ADD] lrm_hr_employee_ppe_custom: add new module

---
+++ lrm_hr_employee_ppe_custom/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Lrm Hr Employee Ppe Custom",
+    "summary": """
+        LRM HR Employee PPE Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/lrm-addons",
+    "depends": ["hr_employee_ppe"],
+    "data": ["report/hr_employee_ppe_report_template.xml"],
+}
